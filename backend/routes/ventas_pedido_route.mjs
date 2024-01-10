2379d74fcbe1583a08f849a60e53922cd1eaa929
SaraSanchezA <151571780+SaraSanchezA@users.noreply.github.com>
2024-01-10 12:18:37 -0500

    Update ventas_pedido_route.mjs

--- backend/routes/ventas_pedido_route.mjs
+++ backend/routes/ventas_pedido_route.mjs
@@ -1,12 +1,12 @@
-import { createPedidos, getLastPedidos, getPedidos, } from '../controllers/ventas_pedido_controller.mjs';
+import { createPedidos, getLastPedidos, getPedidos, deletePedidos,updatePedidos} from '../controllers/ventas_pedido_controller.mjs';
 import express from 'express';
 
 const routerVentasPedido = express.Router();
 routerVentasPedido.get('/pedido',getPedidos)
 routerVentasPedido.post('/pedido',createPedidos)
 routerVentasPedido.get('/pedido_last',getLastPedidos)
-
-
+routerVentasPedido.delete('/pedido/:pedidoID', deletePedidos)
+routerVentasPedido.put('/pedido/:pedidoID', updatePedidos)
 
 
 export default routerVentasPedido;

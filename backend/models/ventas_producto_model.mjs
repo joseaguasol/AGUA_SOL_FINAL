dd7eb840f156ec16c1a7a82f85d27c6bb2b10176
SaraSanchezA <151571780+SaraSanchezA@users.noreply.github.com>
2024-01-17 13:26:01 -0500

    Update ventas_producto_model.mjs

--- backend/models/ventas_producto_model.mjs
+++ backend/models/ventas_producto_model.mjs
@@ -1,9 +1,19 @@
 import { db_pool } from "../config.mjs";
 
 const modelProduct = {
-    getProduct:async () => {
+    geAlltProduct:async () => {
         try{
-            const product = await db_pool.any('select nombre,precio,descripcion,foto from ventas.producto');
+            const product = await db_pool.any('select id,nombre,precio,descripcion,foto from ventas.producto');
+            return product;
+        }
+        catch(e){
+            throw new Error(`Error query create:${e}`);
+        }
+    },
+
+    getONEProduct:async (productID) => {
+        try{
+            const product = await db_pool.any('SELECT id,nombre,descripcion FROM ventas.producto WHERE id=$1',[productID]);
             return product;
         }
         catch(e){

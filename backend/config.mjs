19d2e24cd00e712835f9dac1e0fed429c79555d4
Luchin151090 <lgonzalesu@ulasalle.edu.pe>
2024-01-19 23:40:09 -0500

    update

--- backend/config.mjs
+++ backend/config.mjs
@@ -2,13 +2,8 @@ import pgPromise from 'pg-promise';
 
 const pgp = pgPromise();
 
-<<<<<<< HEAD
 const connectionStr = "postgres://postgres:1234@localhost:5432/newsol2024";
-//const connectionStr = "postgresql://postgres:5fDE6cgfda33eB4FFAabABgAggA4Ca-c@viaduct.proxy.rlwy.net:35474/railway";
-=======
-//const connectionStr = "postgres://postgres:1234@localhost:5432/newsol2024";
-const connectionStr = "postgres://aguasol:TntaHgQf9msnfmHXdrQWEXHEt1hut1MC@dpg-cml86oacn0vc739oj51g-a/aguasol_ui5l";
->>>>>>> 94753e9be950841742b2c6bb4a3738deac9d9584
+//const connectionStr = "postgres://aguasol:TntaHgQf9msnfmHXdrQWEXHEt1hut1MC@dpg-cml86oacn0vc739oj51g-a/aguasol_ui5l";
 export const db_pool =  pgp(connectionStr);
 
 try{

eea59f0be7be178c7393f0eeabb0de498307359e
SaraSanchezA <151571780+SaraSanchezA@users.noreply.github.com>
2024-01-30 08:52:11 -0500

    Update ventas_producto_route.mjs

--- backend/routes/ventas_producto_route.mjs
+++ backend/routes/ventas_producto_route.mjs
@@ -5,6 +5,7 @@ const routerVentasProduct = express.Router();
 
 routerVentasProduct.get('/products',getAllProducts),
 routerVentasProduct.get('/products/:productID',getONEProducts)
+//UPDATE DE SARA
 routerVentasProduct.get('/productsbypromo/:promocionId',getProductsPorPromos)
 
 

9924d943dce440a4d7166191ec757204b8a038d6
SaraSanchezA <151571780+SaraSanchezA@users.noreply.github.com>
2024-01-30 11:04:43 -0500

    Update ventas_clientenr_route.mjs

--- backend/routes/ventas_clientenr_route.mjs
+++ backend/routes/ventas_clientenr_route.mjs
@@ -1,10 +1,13 @@
-import { createClientesnr,getClienteNR } from '../controllers/ventas_clientenr_controller.mjs';
+import { createClientesnr,getClienteNR,getLastClientesnr } from '../controllers/ventas_clientenr_controller.mjs';
 import express from 'express';
 
 const routerClienteNR = express.Router();
 
 routerClienteNR.post('/clientenr',createClientesnr)
 routerClienteNR.get('/clientenr',getClienteNR)
+
+//Cambio para hallar el ultimo cliente NR, segun el empleado ID
+routerClienteNR.get('/last_clientenr/:empleadoID',getLastClientesnr)
 
 
 
